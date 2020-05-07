0185e0b337b4709245e49060c3b4e74fcbf91890
ljp1546 <1134700691@qq.com>
2020-05-07 23:32:04 +0800

    完善了621任务调度器的方法--中等--贪心算法

--- src/com/ljp/leecode_cn/greedy/_中等_621_任务调度器.java
+++ src/com/ljp/leecode_cn/greedy/_中等_621_任务调度器.java
@@ -32,6 +32,7 @@ public class _中等_621_任务调度器 {
         int n = 2;
         System.out.println(leastInterval(tasks, n));
         System.out.println(leastInterval2(tasks, n));
+        System.out.println(leastInterval3(tasks, n));
     }
 
     /**
@@ -101,5 +102,101 @@ public class _中等_621_任务调度器 {
             Arrays.sort(hash);
         }
         return count;
+    }
+
+    /**排序
+     * 官方解法一：思路比我好太多了，太优美
+     * @param tasks
+     * @param n
+     * @return
+     * 执行用时 :
+    7 ms, 在所有 Java 提交中击败了53.03%的用户
+    内存消耗 :
+    41.1 MB, 在所有 Java 提交中击败了11.11%的用户
+     */
+    public static int leastInterval3(char[] tasks, int n) {
+        //1. 统计每个字母的频率
+        int[] map = new  int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            map[tasks[i] - 'A']++;
+        }
+        //2. 排序
+        Arrays.sort(map);
+        //3. 从最大一个开始，直到最大一个都为0
+        int time  = 0;
+        while(map[25] > 0){
+            int i = 0;
+            //4. 循环一个周期
+            while(i <= n){
+                //没有任务就结束
+                if(map[25] == 0){
+                    break;
+                }
+                if(i < 26 && map[25 - i] > 0){
+                    map[25 - i]--;
+                }
+                time++;
+                i++;
+            }
+            //5. 再排序
+            Arrays.sort(map);
+        }
+        return time;
+    }
+    //队列方法不写了
+    /**
+     * 官方方法三：设计
+     * @param tasks
+     * @param n
+     * @return
+     * 执行用时 :
+    2 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗 :
+    40.9 MB, 在所有 Java 提交中击败了11.11%的用户
+     */
+    public static int leastInterval4(char[] tasks, int n) {
+        //统计每个出现频率
+        int[] map = new int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            map[tasks[i] - 'A']++;
+        }
+        //排序
+        Arrays.sort(map);
+        //当最大频率为p，则至少需要时间（p-1) * (n + 1) + 1
+        int max_val = map[25] - 1;
+        int idle_slots = max_val * n;//安排好最大频率任务后，剩下的空闲时间
+        for (int i = 24; i >= 0 ; i--) {
+            idle_slots -= Math.min(map[i], max_val);//防止存在多个最大频率时
+        }
+        return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
+    }
+
+    /**
+     * 设计，评论区大佬思路
+     * @param tasks
+     * @param n
+     * @return
+     * 执行用时 :
+    2 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗 :
+    41.7 MB, 在所有 Java 提交中击败了11.11%的用
+     */
+    public static int leastInterval5(char[] tasks, int n) {
+        //统计词频
+        int[] map = new int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            map[tasks[i] - 'A']++;
+        }
+        //排序
+        Arrays.sort(map);
+        //找到最大词频的出现次数
+        int maxCount = 0;
+        for (int i = 25; i >= 0; i--) {
+            if(map[i] != map[25]){
+                break;
+            }
+            maxCount++;
+        }
+        return Math.max((map[25] - 1) * (n + 1) + maxCount, tasks.length);
     }
 }
