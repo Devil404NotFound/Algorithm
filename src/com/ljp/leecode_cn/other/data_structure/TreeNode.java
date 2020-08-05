fc380a894c702be44834378b7b49cc3b84ff3bf4
ljp1546 <1134700691@qq.com>
2020-08-05 11:56:57 +0800

    添加了 数据结构TreeNode

---
+++ src/com/ljp/leecode_cn/other/data_structure/TreeNode.java
@@ -0,0 +1,10 @@
+package com.ljp.leecode_cn.other.data_structure;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
