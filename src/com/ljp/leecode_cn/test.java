4872028369b6567bb82bc726e706e902cdb22cbf
蓝星尘 <1134700691@qq.com>
2020-10-14 11:04:51 +0800

    添加了 1002-查找常用字符【简单】（字符串、计数）（每日一题 2020.10.14）

---
+++ src/com/ljp/leecode_cn/test.java
@@ -0,0 +1,13 @@
+package com.ljp.leecode_cn;
+
+/**
+ * 一个日常测试类
+ * @author ljp
+ * @date 2020/10/14 10:38
+ */
+public class test {
+    public static void main(String[] args) {
+        char ch = (char)('a' + 2);
+        System.out.println(ch);
+    }
+}
