950a2dbffa11149f0569323e5a4bc270b696e68a
蓝星尘 <1134700691@qq.com>
2020-10-22 23:21:03 +0800

    添加了 763-划分字母区间【中等】（贪心算法）（每日一题2020.10.22）

--- src/com/ljp/leecode_cn/greedy/_中等_763_划字母区间.java
+++ src/com/ljp/leecode_cn/greedy/_中等_763_划字母区间.java
@@ -3,7 +3,7 @@ package com.ljp.leecode_cn.greedy;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
+/**每日一题2020.10.22
  * 763. 划分字母区间
  字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一个字母只会出现在其中的一个片段。返回一个表示每个字符串片段的长度的列表。
 
@@ -27,7 +27,7 @@ public class _中等_763_划字母区间 {
     }
 
     /**
-     *
+     *哈希表，贪心算法
      * @param S
      * @return
      * 执行用时 :
@@ -61,15 +61,15 @@ public class _中等_763_划字母区间 {
     }
 
     /**
-     * 官方题解
+     * 官方题解：贪心算法+字母最后出现的下标
      * @param S
      * @return
      */
     public List<Integer> partitionLabels2(String S) {
         int[] last = new int[26];
-        for (int i = 0; i < S.length(); ++i)
+        for (int i = 0; i < S.length(); ++i){
             last[S.charAt(i) - 'a'] = i;
-
+        }
         int j = 0, anchor = 0;
         List<Integer> ans = new ArrayList();
         for (int i = 0; i < S.length(); ++i) {
