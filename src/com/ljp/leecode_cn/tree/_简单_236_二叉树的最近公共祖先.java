2fc28e63c8d70dfa1d047d17a84aebc78823eeeb
Devil404NotFound <1134700691@qq.com>
2020-12-18 18:10:05 +0800

    添加了 236-二叉数的最近公共祖先【简单】（递归）

---
+++ src/com/ljp/leecode_cn/tree/_简单_236_二叉树的最近公共祖先.java
@@ -0,0 +1,97 @@
+package com.ljp.leecode_cn.tree;
+
+import com.ljp.leecode_cn.other.data_structure.TreeNode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/** 【重要】（经常会忘记怎么做）
+ * @author ljp
+ * @date 2020/12/18 17:58
+236. 二叉树的最近公共祖先
+同剑指Offer68
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+
+
+
+
+示例 1:
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出: 3
+解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+示例 2:
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+输出: 5
+解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+
+
+说明:
+
+所有节点的值都是唯一的。
+p、q 为不同节点且均存在于给定的二叉树中。
+ */
+public class _简单_236_二叉树的最近公共祖先 {
+    /**
+     * 官方题解一：递归
+     */
+    TreeNode ans = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p, q);
+        return ans;
+    }
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return false;
+        }
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if((lson && rson) || ((root == p || root == q) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || root == p || root == q;
+    }
+
+    /**
+     * 官方题解二： 存储父节点
+     执行用时：
+     12 ms, 在所有 Java 提交中击败了15.38%的用户
+     内存消耗：
+     40.1 MB, 在所有 Java 提交中击败了94.91%的用户
+     */
+    Map<TreeNode, TreeNode> parent = new HashMap<>(); //建立子-父节点映射
+    Set<TreeNode> set = new HashSet<>(); //存储p遍历过的节点
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        //先遍历一遍p到根节点的每一个节点
+        while(p != null) {
+            set.add(p);
+            p = parent.get(p);
+        }
+        //遍历q到根节点的每一个节点，找到第一个与p相同的父节点，就返回
+        while(q != null && !set.contains(q)) {
+            set.add(q);
+            q = parent.get(q);
+        }
+        return q;
+    }
+    //递归存储左右子树的父节点
+    private void dfs(TreeNode root) {
+        if(root.left != null) {
+            parent.put(root.left, root);
+            dfs(root.left);
+        }
+        if(root.right != null) {
+            parent.put(root.right, root);
+            dfs(root.right);
+        }
+    }
+}
