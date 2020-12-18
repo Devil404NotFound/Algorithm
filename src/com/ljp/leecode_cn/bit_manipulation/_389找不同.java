d5fad1fb18988c5586d844bed1359dac2ea36876
Devil404NotFound <1134700691@qq.com>
2020-12-18 13:00:30 +0800

    添加了 389-找不同【简单】（求和/计数/位运算）（每日一题2020.12.18）

--- src/com/ljp/leecode_cn/bit_manipulation/_389找不同.java
+++ src/com/ljp/leecode_cn/bit_manipulation/_389找不同.java
@@ -1,6 +1,6 @@
 package com.ljp.leecode_cn.bit_manipulation;
 
-/**
+/** 2020.12.18
  * 389. 找不同
  给定两个字符串 s 和 t，它们只包含小写字母。
 
@@ -46,5 +46,71 @@ public class _389找不同 {
             res ^= (int)(t.charAt(i) - 'a');
         }
         return (char)(res + 'a');
+    }
+
+    /**
+     * 位运算 (同官方题解三）
+     * @param s
+     * @param t
+     * @return
+    执行用时：
+    1 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：
+    36.8 MB, 在所有 Java 提交中击败了71.83%的用户
+     */
+    public char findTheDifference2(String s, String t) {
+        int len = s.length();
+        int hash = 0;
+        for(int i = 0; i < len; i++) {
+            hash ^= s.charAt(i);
+            hash ^= t.charAt(i);
+        }
+        hash ^= t.charAt(t.length() - 1);
+        return (char)(hash);
+    }
+
+    /**
+     * 官方题解一：计数
+     * @param s
+     * @param t
+     * @return
+    执行用时：
+    1 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：
+    36.7 MB, 在所有 Java 提交中击败了87.11%的用户
+     */
+    public char findTheDifference3(String s, String t) {
+        int[] cnt = new int[26];
+        for(char c : s.toCharArray()) {
+            ++cnt[c - 'a'];
+        }
+        for(char c : t.toCharArray()) {
+            --cnt[c - 'a'];
+            if(cnt[c - 'a'] < 0) {
+                return c;
+            }
+        }
+        return ' ';
+    }
+
+    /**
+     * 官方题解二：求和
+     * @param s
+     * @param t
+     * @return
+    执行用时：
+    1 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：
+    36.7 MB, 在所有 Java 提交中击败了86.84%的用户
+     */
+    public char findTheDifference4(String s, String t) {
+        int as = 0, at = 0;
+        for(char c : s.toCharArray()) {
+            as += c;
+        }
+        for(char c : t.toCharArray()) {
+            at += c;
+        }
+        return (char)(at - as);
     }
 }
